e0a95e2f845fb76f0be73dcb1d5071335cfc097e
Thiago Adriano <thiago.s.adriano@gmail.com>
2017-06-05 09:08:08 -0300

    iniciação do angular 2

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+const app_module_1 = require("./app.module");
+const Platform = platform_browser_dynamic_1.platformBrowserDynamic();
+Platform.bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
